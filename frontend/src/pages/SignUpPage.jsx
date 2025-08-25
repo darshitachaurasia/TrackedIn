3aabbe1a4308cb00af018b47f6bd894c16429476
Shruti Tiwari <shrutit515@gmail.com>
2025-08-25 21:27:26 +0530

    Updated Landing.jsx with SignedIn/SignedOut and Clerk fixes

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,11 @@
 import { SignUp } from '@clerk/clerk-react';
+import Dashboard from '../components/Dashboard';
 
 export default function SignUpPage() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
       <div className="shadow-lg p-6 rounded-2xl border border-gray-200 bg-white">
-        <SignUp routing="path" path="/sign-up" />
+        <SignUp routing="path" path="/sign-up" redirectUrl="/Dashboard"  />
       </div>
     </div>
   );
